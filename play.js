d918ab488d27ce01f9d1ecd5a89c12b4d95898e9
Kanem <kanemdeng@email.com>
2022-02-27 00:27:50 -0800

    added ctrl c

--- play.js
+++ play.js
@@ -4,5 +4,22 @@ const {net,connect} = require('./client');
 console.log("Connecting ...");
 connect();
 
+const handleUserInput = function(data) {
+  // your code here
+  if (data === '\u0003') {
+    process.exit();
+  }
+  
+};
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
 
